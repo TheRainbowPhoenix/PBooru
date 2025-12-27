3af972cd82f3e722d72403a269f5c83d839fdf57
PC <TheRainbowPhoenix@users.noreply.github.com>
2025-12-27 23:52:56 +0100

    fix ci ?

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.env.NODE_ENV === 'development';
-
-// For project pages, this should be "/PBooru"
-const base = dev ? '' : (process.env.BASE_PATH ?? '/PBooru');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({
-      fallback: 'index.html'
-    }),
-    paths: {
-      base
-    }
-  }
+	kit: {
+		adapter: adapter({
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
 
 export default config;
